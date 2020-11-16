772ff43cafa6ce1faacec59b7d543f7ca771e828
xiongjian <webjsforyou@gmail.com>
2020-11-16 17:06:29 +0800

    feat: 脚手架的基本使用

---
+++ 04.Vue/03.VueCLi/01.vue_cli_test/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <!-- 模板页面：写页面的 -->
+  <div class="app">
+    <h1>hello vue cli</h1>
+    <p class="app-text">{{ num }}</p>
+    <button @click="add">add</button>
+  </div>
+</template>
+
+<script>
+// 写组件配置对象
+export default {
+  name: "App", // 组件名称
+  data() {
+    return {
+      num: 0
+    };
+  },
+  methods: {
+    add() {
+      this.num++;
+    }
+  }
+};
+</script>
+
+<style lang="less">
+* {
+  margin: 0;
+  padding: 0;
+}
+html,
+body {
+  height: 100%;
+}
+
+.app {
+  height: 100%;
+  background-color: yellowgreen;
+  .app-text {
+    color: hotpink;
+  }
+}
+</style>
---
+++ 04.Vue/03.VueCLi/01.vue_cli_test/src/main.js
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import Vue from "vue";
+
+import App from "./App";
+
+new Vue({
+  render: h => h(App),
+}).$mount("#app");
